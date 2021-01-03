84929ec076f7409631146804b87ccc1099493313
mikhail <mz-83@mail.ru>
2021-01-03 21:55:06 +0400

    добавляет миграцию  и модель Plate

---
+++ database/migrations/2021_01_03_174525_create_plates_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plates', function (Blueprint $table) {
+            $table->id();
+            $table->string('artist_name')->index();
+            $table->string('album_title')->index();
+            $table->integer('duration')->index();
+            $table->float('price', 8, 2)->index();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plates');
+    }
+}
